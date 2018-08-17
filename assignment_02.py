acdefa106f0699cebd74b63f0c636dac27d877e6
saurav1785 <saurav1785.cse16@chitkara.edu.in>
2018-08-17 19:53:40 +0530

    my first commit

---
+++ assignment_02.py
@@ -0,0 +1,37 @@
+#Question 1
+#>Answer 1
+a='Hello World'
+print(a)
+
+#Question 2
+#>Answer 2
+str1='Acad'
+str2='View'
+str3=str1+str2
+print(str3)
+
+#Question 3
+#>Answer 3
+x=int(input("Enter first no."))
+y=int(input("Enter second no."))
+z=int(input("Enter third no."))
+print("First no. is %d, Second no. is %d and Third no. is %d" %(x,y,z))
+
+#Question 4
+#>Answer 4
+a='''Let\'s get started'''
+print(a)
+
+#Question 5
+#>Answer 5
+s='Acadview'
+course='Python'
+fees=5000
+print("Name: %s , Course: %s , Fees: %d" %(s,course,fees))
+
+#Question 6
+#>Answer 6
+radius=float(input("Enter radius "))
+pi=3.14
+area=pi*(radius**2)
+print("Area of circle with radius" , radius , " is " , area)
